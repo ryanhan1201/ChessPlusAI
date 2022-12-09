431079b3fcd0a2b1a162f9249dde1a93b84a0258
ryanhan1201 <ryanhan1201@gmail.com>
2022-12-09 13:03:16 -0800

    Starting to do legal move checks

--- board.py
+++ board.py
@@ -1,5 +1,8 @@
 import piece
-
+"""
+Player 1 = White Pieces
+Player 2 = Black Pieces
+"""
 class World():
     LENGTH = 8
     HEIGHT = 8
@@ -13,24 +16,31 @@ class World():
         for r in range(self.HEIGHT):
             eachRow = []
             for c in range(self.LENGTH):
-                eachRow.append(piece.EMPTY)
+                eachRow.append(piece.Empty(r, c))
             self.GRID.append(eachRow)
         #BLACK PIECES
-        self.GRID[7][0] = piece.ROOK
-        self.GRID[7][1] = piece.KNIGHT
-        self.GRID[7][2] = piece.BISHOP
-        self.GRID[7][3] = piece.QUEEN
-        self.GRID[7][4] = piece.KING
-        self.GRID[7][5] = piece.BISHOP
-        self.GRID[7][6] = piece.KNIGHT
-        self.GRID[7][7] = piece.ROOK
-
+        self.GRID[7][0] = piece.Rook(2, 7, 0)
+        self.GRID[7][1] = piece.Knight(2, 7, 1)
+        self.GRID[7][2] = piece.Bishop(2, 7, 2)
+        self.GRID[7][3] = piece.Queen(2, 7, 3)
+        self.GRID[7][4] = piece.King(2, 7, 4)
+        self.GRID[7][5] = piece.Bishop(2, 7, 5)
+        self.GRID[7][6] = piece.Knight(2, 7, 6)
+        self.GRID[7][7] = piece.Rook(2, 7, 7)
+        #Doing Panws
+        for c in range(self.LENGTH):
+            self.GRID[7][c] = piece.Pawn(2, 7, 0)
         #WHITE PIECES
-        self.GRID[0][0] = piece.ROOK
-        self.GRID[0][1] = piece.KNIGHT
-        self.GRID[0][2] = piece.BISHOP
-        self.GRID[0][3] = piece.QUEEN
-        self.GRID[0][4] = piece.KING
-        self.GRID[0][5] = piece.BISHOP
-        self.GRID[0][6] = piece.KNIGHT
-        self.GRID[0][7] = piece.ROOK
+        self.GRID[0][0] = piece.Rook(1, 0, 0)
+        self.GRID[0][1] = piece.Knight(1, 0, 1)
+        self.GRID[0][2] = piece.Bishop(1, 0, 2)
+        self.GRID[0][3] = piece.King(1, 0, 3)
+        self.GRID[0][4] = piece.Queen(1, 0, 4)
+        self.GRID[0][5] = piece.Bishop(1, 0, 5)
+        self.GRID[0][6] = piece.Knight(1, 0, 6)
+        self.GRID[0][7] = piece.Rook(1, 0, 7)
+        #Doing Panws
+        for c in range(self.LENGTH):
+            self.GRID[0][c] = piece.Pawn(1, 0, c)
+
+    
--- piece.py
+++ piece.py
@@ -1,7 +1,89 @@
-class Pawn():
-class Rook():
-class Knight():
-class Bishop():
-class Queen():
-class King():
-class Empty():
+class Pawn(self):
+    PLAYER = ""
+    POS = (0, 0)
+    INITPOS = (0, 0)
+    def __init__(self, player = int, x = int, y = int):
+        self.PLAYER = player
+        self.POS = (x, y)
+        self.INITPOS = (x, y)
+    """
+    Input: current x, y position, and current state of the board
+    Return: A list of all the valid moves
+    """
+    def legalMoves(x = int, y = int, grid = [[]]):
+        
+        
+class Rook(self):
+    PLAYER = ""
+    POS = (0, 0)
+    INITPOS = (0, 0)
+    def __init__(self, player = int, x = int, y = int):
+        self.PLAYER = player
+        self.POS = (x, y)
+        self.INITPOS = (x, y)
+    """
+    Input: current x, y position, and current state of the board
+    Return: A list of all the valid moves
+    """
+    def legalMoves(x, y):
+        
+class Knight(self):
+    PLAYER = ""
+    POS = (0, 0)
+    INITPOS = (0, 0)
+    def __init__(self, player = int, x = int, y = int):
+        self.PLAYER = player
+        self.POS = (x, y)
+        self.INITPOS = (x, y)
+    """
+    Input: current x, y position, and current state of the board
+    Return: A list of all the valid moves
+    """
+    def legalMoves(x, y):
+        
+class Bishop(self):
+    PLAYER = ""
+    POS = (0, 0)
+    INITPOS = (0, 0)
+    def __init__(self, player = int, x = int, y = int):
+        self.PLAYER = player
+        self.POS = (x, y)
+        self.INITPOS = (x, y)
+    """
+    Input: current x, y position, and current state of the board
+    Return: A list of all the valid moves
+    """
+    def legalMoves(x, y):
+        
+class Queen(self):
+    PLAYER = ""
+    POS = (0, 0)
+    INITPOS = (0, 0)
+    def __init__(self, player = int, x = int, y = int):
+        self.PLAYER = player
+        self.POS = (x, y)
+        self.INITPOS = (x, y)
+    """
+    Input: current x, y position, and current state of the board
+    Return: A list of all the valid moves
+    """
+    def legalMoves(x, y):
+        
+class King(self):
+    PLAYER = ""
+    POS = (0, 0)
+    INITPOS = (0, 0)
+    def __init__(self, player, x, y):
+        self.PLAYER = player
+        self.POS = (x, y)
+        self.INITPOS = (x, y)
+    """
+    Input: current x, y position, and current state of the board
+    Return: A list of all the valid moves
+    """
+    def legalMoves(x, y):
+        
+class Empty(self):
+    POS = (0, 0)
+    def __init(self, x = int, y = int):
+        self.POS = (x, y)
